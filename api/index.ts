8ba22b377e6ebb59c55beb7e83956455d8ce6ff5
PleBea <bini060315kim@gmail.com>
2022-09-24 04:07:30 +0900

    create api

--- api/index.ts
+++ api/index.ts
@@ -1,31 +1,50 @@
 // axios
-import axios from 'axios'
-import type { UserLoginForm, UserRegisterForm } from '../types'
+import axios from 'axios';
+import type { UserLoginForm, UserRegisterForm } from '../types';
 
-const getAPI = axios.create({
+export const getAPI = axios.create({
   baseURL: process.env.API_URL,
   headers: {
     'Content-Type': 'application/json',
-    'Accept': 'application/json'
-  }
-})
+    Accept: 'application/json',
+  },
+});
 
-export async function postRegister (data: UserLoginForm) {
-    const res = await getAPI.post('/auth/register', data).then(res => res).catch(err => err.response)
-    return res
+export async function postRegister(data: UserRegisterForm) {
+  const res = await getAPI
+    .post('/auth/register', data)
+    .then((res) => res)
+    .catch((err) => err.response);
+  return res;
 }
 
-export async function postLogin (data: UserRegisterForm) {
-    const res = await getAPI.post('/auth/login', data).then(res => res).catch(err => err.response)
-    return res
+export async function postLogin(data: UserLoginForm) {
+  const res = await getAPI
+    .post('/auth/login', data)
+    .then((res) => {
+      return res;
+    })
+    .catch((err) => err.response);
+  return res;
 }
 
-export async function forgotPassword (email: string) {
-    const res = await getAPI.post('/auth/forgotPassword', email).then(res => res).catch(err => err.response)
-    return res
+export async function getUserData() {
+  const res = await getAPI.get('/auth/user').catch((err) => err.response);
+  return res;
 }
 
-export async function resetPassword (data: {token:string; password:string}) {
-    const res = await getAPI.post('/auth/resetPassword', data).then(res => res).catch(err => err.response)
-    return res
+export async function forgotPassword(email: string) {
+  const res = await getAPI
+    .post('/auth/forgotPassword', email)
+    .then((res) => res)
+    .catch((err) => err.response);
+  return res;
+}
+
+export async function resetPassword(data: { token: string; password: string }) {
+  const res = await getAPI
+    .post('/auth/resetPassword', data)
+    .then((res) => res)
+    .catch((err) => err.response);
+  return res;
 }
